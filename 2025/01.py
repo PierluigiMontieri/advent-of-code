c4c1c690c43cfd8fd6fe2a58d3ca1194a3b77614
Pierluigi Montieri <pierluigi2001@gmail.com>
2025-12-28 01:21:23 +0100

    day 01 pt2

--- 2025/01.py
+++ 2025/01.py
@@ -1,13 +1,32 @@
 input_folder = "input/"
-pointer = 50
-password = 0
 
 with open(input_folder + "01.txt") as file:
     rotations = [int(line[1:]) * (1 if line[0] == 'R' else -1) for line in file]
 
+# part 1
+pointer = 50
+password = 0
 for rotation in rotations:
     pointer = (pointer + rotation) % 100
     if pointer == 0:
         password += 1
-
 print(password)
+
+# part 2
+pointer = 50
+password = 0
+for rotation in rotations:
+    if rotation > 0:
+        password += (pointer + rotation) // 100
+    else:
+        password -= ((pointer + rotation) // 100)
+        if (pointer + rotation) % 100 == 0:
+            password += 1
+        if pointer == 0:
+            password -= 1
+
+    pointer = (pointer + rotation) % 100
+print(password)
+
+    
+
